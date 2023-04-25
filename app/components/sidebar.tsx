1fe26caf0baaf65ab4b001932a99017adc9fcab8
robinlovoy <w.h.j@126.com>
2023-04-25 07:11:45 +0000

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -90,9 +90,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>ChatGPT WU</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          看见未来.
         </div>
         <div className={styles["sidebar-logo"]}>
           <ChatGptIcon />
@@ -123,11 +123,11 @@ export function SideBar(props: { className?: string }) {
               <IconButton icon={<SettingsIcon />} shadow />
             </Link>
           </div>
-          <div className={styles["sidebar-action"]}>
+          <!-- div className={styles["sidebar-action"]}>
             <a href={REPO_URL} target="_blank">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
-          </div>
+          </div  -->
         </div>
         <div>
           <IconButton
